c736b70e50edc1ff8d2a565f4a97e0bfdf4ef238
14python88 <14python88@proton.me>
2026-06-07 06:47:45 +0000

    Fixed the issue with main() not registering MissionProcessor properly: added the missing variables and method asrgs to the MissionProcessor.hpp

--- homework_08/include/MissionProcessor.hpp
+++ homework_08/include/MissionProcessor.hpp
@@ -14,13 +14,39 @@ class MissionProcessor {
     float getTotalTime(const DroneConfig& config, const float& current_speed, const float& angle, const float& distance, const float& acceleration, const float& t_acceleration);
 
 public:
+
+    int target_count;
+    int time_steps;
+    DroneConfig config;
+    Ammo ammo;
+    std::vector<std::vector<Coord>> targets;
+    DroneState state;
+    PrefParameters prefParameters;
+    float delta_target_bomb;
+    float current_direction;
+    float current_speed;
+    float current_time;
+    float acceleration;
+    float t_acceleration;
+    Coord bombLand;
+    Coord dronePosNow;
+
+
     MissionProcessor(ITargetProvider* t, IConfigLoader* l, IBallisticSolver* s);
     void init();
     ResultConst solve(const DroneConfig droneConfig, const Ammo& ammo);
     Coord calculateBallistics(const Coord& targetPos, const float& h);
     Params calculateParameters(const Coord& dropPos);
-    void interpolateTargets();
-    void extrapolateTargets();
+    void interpolateTargets(
+        std::vector<Coord>& targetPosNow,
+        const int& sim_step,
+        const ResultConst& resultConst
+    );
+    void extrapolateTargets(
+        std::vector<Coord>& targetPosPredicted,
+        const std::vector<Coord>& targetPosNow,
+        const std::vector<Params>& params
+    );
     void updatePrefParams (
         const std::vector<Params>& params,
         const std::vector<Coord>& targetPosPredicted,
